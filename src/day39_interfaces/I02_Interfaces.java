3158aba9c8683f17c1418334943013f1b43692ef
ertugrulkececii <ertgrlkcc@gmail.com>
2023-08-21 23:06:36 +0200

    21-8-monday

---
+++ src/day39_interfaces/I02_Interfaces.java
@@ -0,0 +1,14 @@
+package day39_interfaces;
+
+public interface I02_Interfaces {
+
+    int sayi1 = 200;
+    int sayi4 = 400;
+    int sayi8 = 800;
+
+    abstract int method11();
+    abstract String method3();
+
+    abstract void method44();
+
+}
---
+++ src/day40_interfaces/C01_ChildOfInterfaces.java
@@ -0,0 +1,30 @@
+package day40_interfaces;
+
+
+import day39_interfaces.I01_Interfaces;
+import day39_interfaces.I02_Interfaces;
+
+public class C01_ChildOfInterfaces implements I01_Interfaces, I02_Interfaces{
+
+
+    public void method1() {
+    }
+
+    public int method2() {
+        return 0;
+    }
+
+    public int method11() {
+        return 0;
+    }
+
+    public String method3() {
+        return null;
+    }
+
+    public void method44() {
+    }
+
+    public void method4() {
+    }
+}
