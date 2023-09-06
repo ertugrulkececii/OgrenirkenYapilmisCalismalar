f09ce2d80dd3cb0eb63f2d9c54477d236d1d22fc
ertugrulkececii <ertgrlkcc@gmail.com>
2023-09-06 12:18:55 +0200

    renieuwe

--- src/day43_collection/C04_HashSet.java
+++ src/day43_collection/C04_HashSet.java
@@ -13,14 +13,15 @@ public class C04_HashSet {
     /*
     Soru : Bir HashSet ve TreeSet olusturun. Bir loop ile bu set’lere element ekleyip sureleri karsilastirin.
      */
-    HashSet<Integer> hashSet=new HashSet<>();
+        HashSet<Integer> hashSet = new HashSet<>();
 
-    TreeSet<Integer> treeSet=new TreeSet<>();
+        TreeSet<Integer> treeSet = new TreeSet<>();
 
-    Random rnd= new Random();
+        Random rnd = new Random();
 
-    int hasBas= LocalTime.now().getNano();
-        for (int i = 1; i <10000 ; i++) {
+        int hasBas = LocalTime.now().getNano();
+
+        for (int i = 1; i < 10000; i++) {
             hashSet.add(rnd.nextInt(1000));
         }
 
